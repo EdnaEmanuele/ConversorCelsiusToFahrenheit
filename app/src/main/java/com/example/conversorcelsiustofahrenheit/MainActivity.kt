c6ccd9db5e0e28490ba1f2dc4e9816fa0030d885
EdnaEmanuele <87340605+EdnaEmanuele@users.noreply.github.com>
2024-02-27 15:32:06 +0000

    convert

--- app/src/main/java/com/example/conversorcelsiustofahrenheit/MainActivity.kt
+++ app/src/main/java/com/example/conversorcelsiustofahrenheit/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.conversorcelsiustofahrenheit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.conversorcelsiustofahrenheit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.convertBTN.setOnClickListener{
+            val celsius = binding.editTextNumber.text.toString().toDoubleOrNull()
+            if (celsius != null) {
+                val fahrenheit = celsius * 9 / 5 + 32
+
+                binding.fahrenheit.text = String.format("%.2f", fahrenheit)
+            } else {
+                binding.fahrenheit.text = "Invalid input"
+            }
+        }
     }
 }
